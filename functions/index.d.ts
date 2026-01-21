f33e901cc5feb67d6483c8008e127a773dd8a73f
brittytino <brittytino08@gmail.com>
2026-01-21 21:59:30 +0530

    chore: production ready release

---
+++ functions/index.d.ts
@@ -0,0 +1,4 @@
+import * as functions from "firebase-functions/v1";
+export declare const beforeCreateUser: functions.BlockingFunction;
+export declare const onUserCreated: functions.CloudFunction<import("firebase-admin/auth").UserRecord>;
+//# sourceMappingURL=index.d.ts.map
